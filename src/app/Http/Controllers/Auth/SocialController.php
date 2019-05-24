7989c819c08eeed25235a50d8e540e94012a38d8
aze-git <49616284+aze-git@users.noreply.github.com>
2019-05-24 08:32:41 +0800

    Add facebook impl.

--- src/app/Http/Controllers/Auth/SocialController.php
+++ src/app/Http/Controllers/Auth/SocialController.php
@@ -15,16 +15,16 @@ class SocialController extends Controller
 
     public function getFacebookAuthCallback() {
         $facebook_user = Socialite::driver('facebook')->user();
-        dd($facebook_user);
+        //dd($facebook_user);
         //ToDo: 外側に出してクラス変数にする
         $now = date("Y/m/d H:i:s");
             //ToDo: Facebookのユーザ名をddで確認して、修正する。
             //ToDo: DBのカラム名を変更する。
-            $app_user = DB::select('select * from public.users where github_id = ?', [$github_user->user['login']]);
+            $app_user = DB::select('select * from public.users where github_id = ?', $facebook_user->id);
             if (empty($app_user)) {
-                DB::insert('insert into public.users (github_id, created_at, updated_at) values (?, ?, ?)', [$github_user->user['login'], $now, $now]);
+                DB::insert('insert into public.users (github_id, created_at, updated_at) values (?, ?, ?)', [$facebook_user->id, $now, $now]);
             }
-            $user = DB::table('users')->where('github_id', $github_user->nickname)->first();
+            $user = DB::table('users')->where('github_id', $facebook_user->id)->first();
             $user_id = $user->id;
             $request->session()->put('user_id', $user_id);
             
